c8c5efc5b6e72bc7517e7b45cb17bb164041d284
igna-99 <ignaciovarela7765@gmail.com>
2023-10-24 17:18:58 -0300

    NavBar v1 + capsLock alert in login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,134 +1,161 @@
 <template>
-    <div v-if="usrStore.isLogged" class="">
-        <h1>you are already logged in</h1>
-        <button type="submit" class="" @click="salir">cerrar</button>
-    </div>
+	<div v-if="usrStore.isLogged" class="">
+		<h1>you are already logged in</h1>
+		<button type="submit" class="" @click="salir">cerrar</button>
+	</div>
 
-    <div v-else class="container_grid tamaño_xs">
+	<div v-else class="container_grid tamaño_xs">
 
-        <div class="titulo_container">
-            Iniciar Sesion
-        </div>
+		<div class="titulo_container">
+			Iniciar Sesion
+		</div>
 
-        <div class="formulario_container">
+		<div class="formulario_container">
 
-            <div class="form-floating mb-3 mt-3">
-                <input type="email" class="form-control" id="emailInput" :class="this.isInvalid"
-                    placeholder="name@example.com" v-model="this.email">
-                <label for="emailInput">Email</label>
-            </div>
+			<div class="form-floating mb-3 mt-3">
+				<input type="email" class="form-control" id="emailInput" :class="this.isInvalid" placeholder="name@example.com"
+					v-model="this.email">
+				<label for="emailInput">Email</label>
+			</div>
 
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="passwordInput" :class="this.isInvalid"
-                    placeholder="name@example.com" v-model="this.clave">
-                <label for="passwordInput">Clave</label>
-            </div>
+			<div class="form-floating mb-3">
+				<input type="password" class="form-control " id="passwordInput" :class="this.isInvalid"
+					placeholder="name@example.com" v-model="this.clave" @keyup="isMayus($event)">
 
-            <div class="container_buttons">
-                <button type="" class="btn btn-secondary" @click="">recuperar Clave</button>
-                <button type="" class="btn btn-macabi1" @click="ingresar">Iniciar Sesión</button>
-            </div>
+				<label for="passwordInput">Clave</label>
 
-            <div v-if="this.error" class="alert alert-danger" role="alert">
-                {{ this.msjError }}
-            </div>
+				<svg xmlns="http://www.w3.org/2000/svg" id="pete" width="16" height="16" fill="currentColor"
+					class="bi bi-capslock-fill capslock" viewBox="0 0 16 16">
+					<path
+						d="M7.27 1.047a1 1 0 0 1 1.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H11.5v1a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-1H1.654C.78 9.5.326 8.455.924 7.816L7.27 1.047zM4.5 13.5a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-1z" />
+				</svg>
 
-        </div>
+			</div>
 
-    </div>
+			<div class="container_buttons">
+				<button type="" class="btn btn-secondary" @click="">recuperar Clave</button>
+				<button type="" class="btn btn-macabi1" @click="ingresar">Iniciar Sesión</button>
+			</div>
+
+			<div v-if="this.error" class="alert alert-danger" role="alert">
+				{{ this.msjError }}
+			</div>
+
+		</div>
+
+	</div>
 </template>
 
 <script>
 import { usrStore } from './stores/usrStore.ts'
 
 export default {
-    data() {
-        return {
-            usrStore: usrStore(),
-            email: "",
-            clave: "",
-            error: false,
-            msjError: "",
-        };
-    },
-    async created() {
+	data() {
+		return {
+			usrStore: usrStore(),
+			email: "",
+			clave: "",
+			error: false,
+			msjError: "",
+			capslock: ""
+		};
+	},
+	async created() {
 
-    },
-    computed: {
+	},
+	computed: {
 
-        isInvalid() {
-            let msj
-            if (this.error == true) {
-                msj = 'is-invalid'
-            }
-            return msj
-        }
+		isInvalid() {
+			let msj
+			if (this.error == true) {
+				msj = 'is-invalid'
+			}
+			return msj
+		}
 
-    },
-    methods: {
-        async ingresar() {
+	},
+	methods: {
+		async ingresar() {
 
-            if (this.email == "" || this.clave == "") {
+			if (this.email == "" || this.clave == "") {
 
-                this.error = true;
-                this.msjError = `email o contraseña no ingreados`
+				this.error = true;
+				this.msjError = `email o contraseña no ingreados`
 
-            } else {
+			} else {
 
-                let mensajeError = await this.usrStore.logIn(this.email, this.clave);
+				let mensajeError = await this.usrStore.logIn(this.email, this.clave);
 
-                if (mensajeError == null) {
-                    this.error = false;
+				if (mensajeError == null) {
+					this.error = false;
 
-                    this.email = ""
-                    this.clave = ""
+					this.email = ""
+					this.clave = ""
 
-                } else {
+				} else {
 
-                    this.error = true;
-                    this.msjError = mensajeError;
-                }
-            }
-        },
-        salir() {
-            // metodo salir de usuario
-            this.usrStore.logOut()
-        }
+					this.error = true;
+					this.msjError = mensajeError;
+				}
+			}
+		},
+		isMayus(event) {
+			const capsLockEnabled = event.getModifierState("CapsLock");
+
+			if (capsLockEnabled) {
+				document.getElementById('pete').style.display = 'inherit'
+
+			} else {
+				document.getElementById('pete').style.display = 'none'
+			}
+		},
+		salir() {
+			// metodo salir de usuario
+			this.usrStore.logOut()
+		},
 
 
-    }
+
+	}
 };
 
 </script>
 
 <style scoped>
+.capslock {
+	display: none;
+	position: absolute;
+	right: 40px;
+	top: 21px
+}
+
 .container_grid {
-    grid-template-rows: 1fr 3fr;
+	grid-template-rows: 1fr 3fr;
 }
 
 .btn-macabi1 {
-    color: white;
-    background-color: #004896;
-    border: 1px solid #013a77;
+	color: white;
+	background-color: #004896;
+	border: 1px solid #013a77;
 }
 
 .btn-macabi1:hover {
-    color: white;
-    background-color: #013368;
+	color: white;
+	background-color: #013368;
 }
 
-.btn-macabi1:active  {
-    background-color: #002b58; 
+.btn-macabi1:active {
+	background-color: #002b58;
 }
 
 .alert {
-    text-align: center;
+	text-align: center;
 }
 
 @media screen and (max-width:700px) {
-    .container_buttons {
-        flex-direction: column;
-    }
+	.container_buttons {
+		flex-direction: column;
+	}
 }
 </style>
 
--- src/utils/Store.ts
+++ src/utils/Store.ts
@@ -2,99 +2,119 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 
 export function useElementStore(nombreStore) {
-    return defineStore(nombreStore, {
-        state: () => ({
-            elements: null,
-            currentElement: null,
-        }),
+	return defineStore(nombreStore, {
+		state: () => ({
+			elements: null,
+			elementsList: null,
+			currentElement: null,
+		}),
 
-        getters: {
-            getElements() {
-                return this.elements;
-            },
+		getters: {
+			getElements() {
+				return this.elements;
+			},
 
-            getElementById() {
-                return (id) => this.elements.result.find((e) => e.id === id);
-            },
+			getElementsList() {
+				return this.elementsList;
+			},
 
-            getWithKeyValue() {
-                return (key, value) => this.elements.result.find((e) => {
-                    if(e.hasOwnProperty(key)){
-                        return e[key] == value
-                    } else {
-                        return false
-                    }
-                });
-            },
+			getElementById() {
+				return (id) => this.elements.result.find((e) => e.id === id);
+			},
 
-        },
+			getWithKeyValue() {
+				return (key, value) => this.elements.result.find((e) => {
+					if (e.hasOwnProperty(key)) {
+						return e[key] == value
+					} else {
+						return false
+					}
+				});
+			},
 
-        actions: {
-            
-            async fetchElements(url) {
-                if (true) {  //pongo true porque sino no recarga nunca :) despues vemos que hacemos
-                    try {
-                        const response = await axios.get(url, { withCredentials: true })
-                        this.elements = response.data
-                        //console.log(response.data)
-                    } catch (error) {
-                        console.error('Error fetching elements:', error)
-                    }
-                }
-            },
+		},
 
-            setCurrentElement(value) {
-                this.currentElement = value;
-            },
+		actions: {
 
-            setElements(value) {
-                this.elements = value;
-            },
+			async fetchElements(url) {
+				if (true) {  //pongo true porque sino no recarga nunca :) despues vemos que hacemos
+					try {
+						const response = await axios.get(url, { withCredentials: true })
+						this.elements = response.data
+						this.elementsList = response.data.result
+						//console.log(response.data)
+					} catch (error) {
+						console.error('Error fetching elements:', error)
+					}
+				}
+			},
 
-            async fetchElementById(url, id) {
-                try {
-                    const response = await axios.get(`${url}/${id}`, { withCredentials: true })
-                    this.currentElement = response.data
-                } catch (error) {
-                    console.error(`Error fetching element with id ${id}:`, error)
-                }
-            },
+			setCurrentElement(value) {
+				this.currentElement = value;
+			},
 
-            async createElement(url, newElement) {
-                try {
-                    const response = await axios.post(`${url}`, newElement)
-                    //this.elements.push(response.data)
-                } catch (error) {
-                    console.error('Error creating element:', error)
-                }
-            },
+			setElements(value) {
+				this.elements = value;
+			},
 
-            async updateElement(url, updatedElement, idField) {
-                try {
-                    //console.log(updatedElement)
-                    const response = await axios.put(`${url}/${updatedElement[idField]}`, updatedElement);
-                } catch (error) {
-                    console.error(`Error updating Element with ${idField} ${updatedElement[idField]}:`, error);
-                }
-            },      
+			async fetchElementById(url, id) {
+				try {
+					const response = await axios.get(`${url}/${id}`, { withCredentials: true })
+					this.currentElement = response.data
+				} catch (error) {
+					console.error(`Error fetching element with id ${id}:`, error)
+				}
+			},
 
-            async deleteElement(url, id) {
-                try {
-                    await axios.delete(`${url}/${id}`)
-                    //this.elements = this.elements.filter((e) => e.id !== id)
-                } catch (error) {
-                    console.error(`Error deleting Element with id ${id}:`, error)
-                }
-            },
+			async createElement(url, newElement) {
+				try {
+					const response = await axios.post(`${url}`, newElement)
+					//this.elements.push(response.data)
+				} catch (error) {
+					console.error('Error creating element:', error)
+				}
+			},
 
-            confirm(accion: string, modificacion: string, tipo: string) {
-                var confirmado = true;
-                var confirmar = window.confirm(`¿Estás seguro de ${accion} este ${tipo}?`);
-                if (confirmar) {
-                    alert(`${tipo} ${modificacion} correctamente`);
-                } else { confirmado = false; }
-                return confirmado;
-            },
-        }
-    })
+			async updateElement(url, updatedElement, idField) {
+				try {
+					//console.log(updatedElement)
+					const response = await axios.put(`${url}/${updatedElement[idField]}`, updatedElement);
+				} catch (error) {
+					console.error(`Error updating Element with ${idField} ${updatedElement[idField]}:`, error);
+				}
+			},
+
+			async deleteElement(url, id) {
+				try {
+					await axios.delete(`${url}/${id}`)
+					//this.elements = this.elements.filter((e) => e.id !== id)
+				} catch (error) {
+					console.error(`Error deleting Element with id ${id}:`, error)
+				}
+			},
+
+			confirm(accion: string, modificacion: string, tipo: string) {
+				var confirmado = true;
+				var confirmar = window.confirm(`¿Estás seguro de ${accion} este ${tipo}?`);
+				if (confirmar) {
+					alert(`${tipo} ${modificacion} correctamente`);
+				} else { confirmado = false; }
+				return confirmado;
+			},
+
+			filtrarXString(busqueda: string | number, tipoFiltro: string) {
+				this.elements = this.elements.filter(item => {
+					const propiedad = item[tipoFiltro];
+					if (typeof propiedad === "string" || typeof propiedad === "number") {
+						const propiedadLowerCase = String(propiedad).toLowerCase();
+						const busquedaLowerCase = String(busqueda).toLowerCase();
+						return propiedadLowerCase.includes(busquedaLowerCase);
+					}
+					return false;
+				});
+			}
+
+
+		}
+	})
 }
