0e5b58345a76e3a01b2b1161c9d4e77af2341bfc
Gino Basile <basilegino@hotmail.com>
2023-10-25 15:19:26 -0300

    Agrego logica para mostrar profe, y un estilo a la lista

--- src/components/views/Fechas/DetalleFecha.vue
+++ src/components/views/Fechas/DetalleFecha.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="container mt-3">
     <div class="text text-center pb-3 pt-5 h1">Detalles de la Fecha</div>
-    
+
     <div>
-       
       <!-- Muestra los datos de la fecha -->
       <div class="fecha-details">
         <h2>Fecha: {{ fechaDetalle.fechaCalendario }}</h2>
+        <p>Profesor Asignado: {{profesor}} </p>
         <p>Tipo: {{ mapearTipo(fechaDetalle.tipo) }}</p>
-        <p>Categoria: {{ fechaDetalle.Categorium.nombreCategoria}}</p>
+        <p>Categoria: {{ fechaDetalle.Categorium.nombreCategoria }}</p>
         <p>Deporte: {{ deporte }}</p>
       </div>
       <div>
-        <h2>Socios Anotados en esta Fecha: <strong>{{ size }}</strong></h2>
+        <h2>
+          Socios Anotados en esta Fecha: <strong>{{ size }}</strong>
+        </h2>
         <table class="table table-striped table-bordered">
           <thead>
             <tr>
@@ -25,7 +27,7 @@
             <tr v-for="socio in sociosAsistenciaFecha" :key="socio.idSocio">
               <td>{{ socio.nombreSocio }}</td>
               <td>{{ socio.apellidoSocio }}</td>
-              <td>{{mapearEstado( socio.estado )}}</td>
+              <td>{{ mapearEstado(socio.estado) }}</td>
             </tr>
           </tbody>
         </table>
@@ -36,97 +38,113 @@
     </div>
     <div class="d-flex justify-content-center align-items-center">
       <button class="btn btn-secondary">
-        <router-link to="/" class="nav-item nav-link" href="#">Volver a Inicio</router-link>
+        <router-link to="/" class="nav-item nav-link" href="#"
+          >Volver a Inicio</router-link
+        >
       </button>
     </div>
   </div>
 </template>
 
 <script>
-import { useElementStore } from '../../../utils/Store';
+import { useElementStore } from "../../../utils/Store";
 import { onBeforeMount, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import apiUrl from '../../../../config/config.js';
+import apiUrl from "../../../../config/config.js";
 
 export default {
   setup() {
-    const asistenciaStore = useElementStore("asistencias")()
-    const fechaStore = useElementStore("fechas")()
-    const sociosAsistenciaFecha = ref([])
-    const fechaDetalle = ref([])
+    const asistenciaStore = useElementStore("asistencias")();
+    const fechaStore = useElementStore("fechas")();
+    const sociosAsistenciaFecha = ref([]);
+    const fechaDetalle = ref([]);
     const route = useRoute();
     const router = useRouter();
-    const idFecha = route.params.id
-    const size = ref(0)
-    const deporte = ref("")
-    const showError = ref(false);
-    onBeforeMount(async () => { await fetchs() })
+    const idFecha = route.params.id;
+    const size = ref(0);
+    const deporte = ref("");
+    const profesor = ref("");
+    onBeforeMount(async () => {
+      await fetchs();
+    });
 
     async function fetchs() {
       try {
-        await asistenciaStore.fetchElements(`${apiUrl}/asistencia/${idFecha}`)
-        sociosAsistenciaFecha.value = asistenciaStore.getElements.result
-        size.value = sociosAsistenciaFecha.value.length
+        await asistenciaStore.fetchElements(`${apiUrl}/asistencia/${idFecha}`);
+        sociosAsistenciaFecha.value = asistenciaStore.getElements.result;
+        size.value = sociosAsistenciaFecha.value.length;
       } catch (error) {
-        console.error("Error fetching socios anotados:", error)
-        
+        console.error("Error fetching socios anotados:", error);
       }
 
       try {
-        await fechaStore.fetchElements(`${apiUrl}/fecha/fechas/${idFecha}`)
-        fechaDetalle.value = fechaStore.getElements.result[0]
+        await fechaStore.fetchElements(`${apiUrl}/fecha/fechas/${idFecha}`);
+        fechaDetalle.value = fechaStore.getElements.result[0];
 
         if (fechaDetalle.value && fechaDetalle.value.idCategoria) {
-      const idCategoria = fechaDetalle.value.idCategoria;
-      deporte.value = await obtenerDeporte(idCategoria);
-    }
-
+          const idCategoria = fechaDetalle.value.idCategoria;
+          deporte.value = await obtenerDeporte(idCategoria);
+          profesor.value = await obtenerProfesor(idCategoria);
+        }
       } catch (error) {
-        console.error("Error fetching fecha detalle:", error)
+        console.error("Error fetching fecha detalle:", error);
       }
-
     }
-    
+
     const obtenerDeporte = async (idCategoria) => {
       try {
-        const response = await fetch(`${apiUrl}/categoria/${idCategoria}/nombreDeporte`);
+        const response = await fetch(
+          `${apiUrl}/categoria/${idCategoria}/nombreDeporte`
+        );
         if (response.ok) {
           const data = await response.json();
           return data.nombreDeporte;
         } else {
-          console.error('Error al obtener el nombre del deporte');
+          console.error("Error al obtener el nombre del deporte");
           return null;
         }
       } catch (error) {
-        console.error('Error en la solicitud:', error);
+        console.error("Error en la solicitud:", error);
         return null;
       }
     };
-    
 
+    const obtenerProfesor = async (idCategoria) => {
+      try {
+        const response = await fetch(`${apiUrl}/usuario/${idCategoria}`);
+        if (response.ok) {
+          const data = await response.json();
+          console.log("EEEE", data)
+          return data.result.nombre + " " + data.result.apellido;
+        } else {
+          console.error("Error al obtener nombre del profesor");
+          return null;
+        }
+      } catch (error) {
+        console.error("Error en la solicitud:", error);
+        return null;
+      }
+    };
 
     function mapearTipo(tipo) {
-      if (tipo === 'C') {
-        return 'Citación';
-      } else if (tipo === 'E') {
-        return 'Entrenamiento';
+      if (tipo === "C") {
+        return "Citación";
+      } else if (tipo === "E") {
+        return "Entrenamiento";
       } else {
-        return 'Tipo Desconocido';
+        return "Tipo Desconocido";
       }
     }
 
     function mapearEstado(estado) {
-        if (estado == 'P'){
-            return 'Presente';
-        }else if(estado == 'A'){
-            return 'Ausente';
-        }else if(estado == 'L'){
-            return "Lesionado"
-        }
-        }
-
-      
-    
+      if (estado == "P") {
+        return "Presente";
+      } else if (estado == "A") {
+        return "Ausente";
+      } else if (estado == "L") {
+        return "Lesionado";
+      }
+    }
 
     return {
       sociosAsistenciaFecha,
@@ -136,46 +154,37 @@ export default {
       mapearTipo,
       obtenerDeporte,
       deporte,
-      
-
-    }
-  }
-}
+      profesor
+    };
+  },
+};
 </script>
 
 <style scoped>
 .fecha-details {
+  border: 6px solid #013a77;
   background-color: #f0f0f0;
   padding: 10px;
   margin: 10px 0;
 }
 
+
+.table-bordered {
+  border: 6px solid #013a77; 
+}
+
+
+.table-bordered th,
+.table-bordered td {
+  border: 1px solid #013a77; 
+}
+
+
+
+
 .no-socios {
   color: red;
 }
 
-.error-container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(255, 0, 0, 0.8); /* Fondo rojo semi-transparente */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 999; /* Asegura que esté por encima de otros elementos */
-}
 
-.error-message {
-  text-align: center;
-  background-color: #fff; /* Fondo blanco */
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0px 0px 10px 0px #000; /* Sombra */
-}
-
-.error-text {
-  font-size: 18px;
-}
 </style>
