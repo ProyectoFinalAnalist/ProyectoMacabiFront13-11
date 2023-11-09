9df81cc0ccadce1a95ccb66ee54e73d82569c0ca
MatiasAltmann <102705202+MatiasAltmann@users.noreply.github.com>
2023-11-08 22:17:25 -0300

    arreglos

--- src/components/views/Fechas/DetalleFecha.vue
+++ src/components/views/Fechas/DetalleFecha.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mt-3">
+  <div class="container mt-3 mb-5">
     <div class="text text-center pb-3 pt-5 h1">Detalles de la Fecha</div>
 
     <div>
@@ -36,14 +36,17 @@
         </div>
       </div>
     </div>
-    <div class="d-flex justify-content-center align-items-center">
+    <div class="d-flex justify-content-center align-items-center mt-5">
       <button class="btn btn-secondary">
         <router-link to="/" class="nav-item nav-link" href="#"
-          >Volver a Inicio</router-link
-        >
+          >Volver a Inicio</router-link  >
       </button>
-      <router-link class="btn btn-primary m-3" :to="`/editarfecha/${fechaDetalle.idFecha}`">Editar Fecha</router-link>
       <button  class="btn btn-dark" @click="asignarAsistencia">Asignar/Modificar asistencias</button>
+
+    </div>
+    <div class="d-flex justify-content-center align-items-center mt-5">
+
+    <router-link class="btn btn-primary m-3" :to="`/editarfecha/${fechaDetalle.idFecha}`">Editar Fecha</router-link>
       <button @click="eliminarFecha" class="btn btn-danger m-3">Eliminar fecha</button>
     </div>
   </div>
@@ -153,9 +156,19 @@ export default {
     }
     function eliminarFecha() {
 
-
+      let idCategoria = route.query.idCategoria;
       fechaStore.deleteElement(apiUrl,"fecha/" + idFecha + "/eliminarFecha")
-      router.go(-1)
+      if(confirm("¿Estas seguro que queres borras definitivamente la fecha con todas sus asistencias existentes?")) {
+        router.push({ path: `/fechasCategoria/${idCategoria}` })
+        alert("Borrado con éxito")
+      }else
+      alert('Se canceló la operación');
+      
+
+
+    
+
+
 
 
       
